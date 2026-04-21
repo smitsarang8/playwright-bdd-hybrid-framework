e9043d5280a6ff31e3ebfd42fdedd2f320b1a2ab
Smit Sarang <smit.sarang@collectiveartists.com>
2026-04-21 19:45:24 +0530

    add CI workflow

--- framework/fixtures/world.ts
+++ framework/fixtures/world.ts
@@ -6,7 +6,9 @@ export class CustomWorld extends World {
   page!: Page;
 
   async init() {
-    this.browser = await chromium.launch({ headless: false });
+    this.browser = await chromium.launch({
+  headless: process.env.CI ? true : false
+});
     const context = await this.browser.newContext();
     this.page = await context.newPage();
   }
